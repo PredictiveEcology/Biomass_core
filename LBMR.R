14ddc3f956fb38bef6f7556c8185712238c23a51
CeresBarros <cbarros@mail.ubc.ca>
2018-08-23 11:29:25 -0700

    Understanding fire regeneration event
    * added comments to explain the mechanisms being simulated

--- LBMR.R
+++ LBMR.R
@@ -1182,6 +1182,9 @@ calcSiteShade <- function(time, cohortData, speciesEcoregion, minRelativeB) {
 }
 
 assignLightProb <- function(sufficientLight, newCohortData) {
+  ## for each line, get the survival probability from sufficientLight table
+  ## note that sufficentLight is a table of survival probs for each tolerance level (row) by and shade level (column)
+  ## siteShade + 2 is necessary to skip the first column
   newCohortData[ , lightProb := sufficientLight[cbind(shadetolerance, siteShade + 2)]]
 }
 
@@ -1193,15 +1196,20 @@ addNewCohorts <- function(newCohortData, cohortData, pixelGroupMap, time, specie
   # newCohortData must have the original pixelgroup, regenerated species and pixelindex
   # it also would be better if it has the collums of cohortData plus pixelindex
   newCohortData$pixelGroup <- getValues(pixelGroupMap)[newCohortData$pixelIndex]
-  set(newCohortData, , "temppixelGroup", as.integer(as.factor(newCohortData$pixelGroup)))
+  set(newCohortData, , "temppixelGroup", as.integer(as.factor(newCohortData$pixelGroup))) 
   set(newCohortData, , "speciesposition", 2^(newCohortData$speciesCode))
+  
   # newCohortDataExtra is used to connect the original pixelGroup to the newPixelGroup
+  # communities are any unique combinations of species
   newCohortDataExtra <- newCohortData[, .(community = sum(speciesposition),
                                           pixelGroup = mean(pixelGroup),
                                           temppixelGroup = mean(temppixelGroup)), by = pixelIndex]
   set(newCohortData, , c("temppixelGroup", "speciesposition"), NULL)
   set(newCohortDataExtra, , "community",
       as.integer(as.factor(newCohortDataExtra$community)))
+  ## make unique ids for the combination of communities and pix groups
+  ## if there more communities, start IDs >max(communities)
+  ## if there more/= pix groups, start IDs >max(pix groups)
   if (max(newCohortDataExtra$community) > max(newCohortDataExtra$temppixelGroup)) {
     set(newCohortDataExtra, ,  "community",
         newCohortDataExtra$community + max(newCohortDataExtra$community)*newCohortDataExtra$temppixelGroup)
@@ -1209,21 +1217,28 @@ addNewCohorts <- function(newCohortData, cohortData, pixelGroupMap, time, specie
     set(newCohortDataExtra, , "community",
         newCohortDataExtra$temppixelGroup + max(newCohortDataExtra$temppixelGroup)*newCohortDataExtra$community)
   }
+
+  ## make new pixel groups by adding community IDs to previous max pix group ID
   maxPixelGroup <- max(max(cohortData$pixelGroup), maxValue(pixelGroupMap))
   set(newCohortDataExtra, ,  "newpixelGroup",
       as.integer(as.factor(newCohortDataExtra$community)) + maxPixelGroup)
   set(newCohortDataExtra, , c("community", "temppixelGroup"), NULL)
   setkey(newCohortData, pixelIndex)
   setkey(newCohortDataExtra, pixelIndex)
-  newCohortData <- newCohortData[,pixelGroup:=NULL][newCohortDataExtra][,pixelIndex := NULL]
+  newCohortData <- newCohortData[,pixelGroup:=NULL][newCohortDataExtra][,pixelIndex := NULL] ## replace old IDs
   newCohortData <- unique(newCohortData, by = c("newpixelGroup", "speciesCode"))
-  sumTable <- cohortData[, .(pixelGroup,sumB)] %>%
+  
+  ## extract total pix group biomass, and join to new data - WHY there are no common pixIDs?
+  sumTable <- cohortData[, .(pixelGroup,sumB)] %>% 
     unique(, by = c("pixelGroup"))
   newCohortData <- dplyr::left_join(newCohortData, sumTable, by = "pixelGroup") %>% data.table()
   newCohortData[is.na(sumB),sumB:=0]
   set(cohortData, ,"sumB", NULL)
   set(newCohortData, , "pixelGroup", newCohortData$newpixelGroup)
   set(newCohortData, , c("newpixelGroup"), NULL)
+  
+  ## get spp "productivity traits" per ecoregion/present year
+  ## calculate maximum biomass per ecoregion, join to new cohort data
   specieseco_current <- speciesEcoregion[year <= time]
   specieseco_current <- setkey(specieseco_current[year == max(specieseco_current$year),
                                                   .(speciesCode, maxANPP,
@@ -1231,7 +1246,8 @@ addNewCohorts <- function(newCohortData, cohortData, pixelGroupMap, time, specie
                                speciesCode, ecoregionGroup)
   specieseco_current[, maxB_eco:=max(maxB), by = ecoregionGroup]
   newCohortData <- setkey(newCohortData, speciesCode, ecoregionGroup)[specieseco_current, nomatch=0]
-  set(newCohortData, , "age", 1)
+  set(newCohortData, , "age", 1)  ## set age to 1
+  ## set biomass - if B=0, it's getting maxANPP ???
   set(newCohortData, ,"B",
       as.integer(pmax(1, newCohortData$maxANPP*exp(-1.6*newCohortData$sumB/newCohortData$maxB_eco))))
   set(newCohortData, ,"B", as.integer(pmin(newCohortData$maxANPP, newCohortData$B)))
@@ -1240,7 +1256,8 @@ addNewCohorts <- function(newCohortData, cohortData, pixelGroupMap, time, specie
                                     mortality = 0, aNPPAct = 0)]
   newCohortDataExtra2 <- unique(newCohortDataExtra, by = c("pixelGroup", "newpixelGroup"))
   # newCohortDataExtra2 is further simplified form
-  # identify which pixelGroups in cohortData have new regeneration
+  # identify which pixelGroups in cohortData have new regeneration ???
+  ## Ceres: its seems to me like this is actually checking for overlapping data 
   existingData <- cohortData[pixelGroup %in% unique(newCohortDataExtra2$pixelGroup)]
   setkey(newCohortDataExtra2, pixelGroup)
   setkey(existingData, pixelGroup)
@@ -1257,6 +1274,7 @@ addNewCohorts <- function(newCohortData, cohortData, pixelGroupMap, time, specie
   cohortData <- cohortData[pixelGroup %in% unique(getValues(pixelGroupMap)),]
   pixelGroupMap_new <- pixelGroupMap
   
+  ## re-write pixel groups from 1 to the number of unique IDs
   temppixelIndex11 <- which(!(getValues(pixelGroupMap) %in% c(0, -1)))
   pgmTemp <- getValues(pixelGroupMap)[temppixelIndex11]
   pixelGroupMap_new[temppixelIndex11] <- as.integer(as.factor(pgmTemp))
