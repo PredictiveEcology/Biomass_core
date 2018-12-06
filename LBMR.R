428d62ca934d8e23b8c8f6a36b57f5369beaef43
Alex Chubaty <alex.chubaty@gmail.com>
2018-12-06 15:18:02 -0700

    add colours to species equivalency table

--- LBMR.R
+++ LBMR.R
@@ -1059,6 +1059,12 @@ CohortAgeReclassification <- function(sim) {
 
     ## By default, Abies_las is renamed to Abies_sp
     sim$speciesEquivalency[KNN == "Abie_Las", LandR := "Abie_sp"]
+
+    ## add default colors for species used in model
+    defaultCols <- RColorBrewer::brewer.pal(6, "Accent")
+    LandRNames <- c("Pice_mar", "Pice_gla", "Popu_tre", "Pinu_sp", "Abie_sp")
+    sim$speciesEquivalency[LandR == LandRNames, cols := defaultCols[-4]]
+    sim$speciesEquivalency[EN_generic_full == "Mixed", cols := defaultCols[4]]
   }
   return(invisible(sim))
 }
