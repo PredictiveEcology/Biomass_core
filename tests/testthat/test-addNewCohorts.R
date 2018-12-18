384ef4c391de13a72414ee5131890cff2d08b52e
Alex Chubaty <alex.chubaty@gmail.com>
2018-12-18 15:33:40 -0700

    cleanup tests and add TODO items

--- tests/testthat/test-addNewCohorts.R
+++ tests/testthat/test-addNewCohorts.R
@@ -1,31 +1,30 @@
 test_that("test add new cohort function",{
   # define the module and path
   module <- list("LBMR")
-  path <- list(modulePath="~/GitHub/nrv-succession/code blitz succession/Module_LBMR",
-               outputPath="~/output")
-  parameters <- list(.progress=list(type="graphical", interval=1),
-                     .globals=list(verbose=FALSE),
-                     LBMR=list( .saveInitialTime=NA))
+  path <- list(modulePath = "~/GitHub/nrv-succession/code blitz succession/Module_LBMR", # TODO: use general path
+               outputPath = "~/output") # TODO: use general path
+  parameters <- list(.progress = list(type = "graphical", interval = 1),
+                     .globals = list(verbose = FALSE),
+                     LBMR = list(.saveInitialTime = NA))
 
-  pixelGroupMap <- raster(xmn=50,xmx=50+3*100,
-                          ymn=50,ymx=50+3*100,
-                          res=c(100,100), val=1)
+  pixelGroupMap <- raster(xmn = 50, xmx = 50 + 3 * 100,
+                          ymn = 50, ymx = 50 + 3 * 100,
+                          res = c(100, 100), val = 1)
   pixelGroupMap[1] <- -1
   pixelGroupMap[2:6] <- 2
 
   cohortData <- data.table(pixelGroup = 1, ecoregionGroup = 1L,
                            speciesCode = 7, age = 31, B = 30000L,
                            mortality = 50,  aNPPAct = 1079.76, sumB = 30000L)
-  newcohortdata1 <- rbindlist(list(cohortData,cohortData,cohortData))[,':='(speciesCode=c(7,1,7),
-                                                                 pixelIndex=c(7:8,8))]
-  newcohortdata2 <- rbindlist(list(cohortData,cohortData,
-                                   cohortData,cohortData))[
-                                     ,':='(pixelGroup=2,
-                                           speciesCode=c(7,7,7,1),
-                                           pixelIndex=c(4:6,6))]
-  newcohortdata <- rbindlist(list(newcohortdata1,newcohortdata2))[,':='(age = NULL, B = NULL,
-                                                                        sumB = NULL, aNPPAct = NULL,
-                                                                        mortality = NULL)]
+  newcohortdata1 <- rbindlist(list(cohortData, cohortData, cohortData))[
+    , ':='(speciesCode = c(7, 1, 7), pixelIndex = c(7:8, 8))]
+  newcohortdata2 <- rbindlist(list(cohortData, cohortData,
+                                   cohortData, cohortData))[
+                                     ,':='(pixelGroup = 2,
+                                           speciesCode = c(7, 7, 7, 1),
+                                           pixelIndex = c(4:6, 6))]
+  newcohortdata <- rbindlist(list(newcohortdata1,newcohortdata2))[
+    , ':='(age = NULL, B = NULL, sumB = NULL, aNPPAct = NULL, mortality = NULL)]
 
   speciesEcoregion <- data.table(year = 0,
                                  maxANPP = c(886L, 1175L, 1106L, 1202L, 1202L, 1202L, 969L, 1130L,
@@ -40,13 +39,8 @@ test_that("test add new cohort function",{
                    modules = module,
                    objects = objects,
                    paths = path)
-  if (exists("addNewCohorts")) {
-    output <- addNewCohorts(newcohortdata, cohortData, pixelGroupMap,
-                            time = time(mySim), speciesEcoregion = speciesEcoregion)
-  } else {
-    output <- mySim$addNewCohorts(newcohortdata, cohortData, pixelGroupMap,
-                                  time = time(mySim), speciesEcoregion = speciesEcoregion)
-  }
+  output <- addNewCohorts(newcohortdata, cohortData, pixelGroupMap,
+                          time = time(mySim), speciesEcoregion = speciesEcoregion)
   mapOutput <- getValues(output$pixelGroupMap)
   mapOutput_compared <- c(-1, 2, 2, 4, 4, 6, 3, 5, 1)
   expect_equal(mapOutput,mapOutput_compared)
@@ -59,5 +53,4 @@ test_that("test add new cohort function",{
                                           B = c(30000L, 225L, 30000L, 969L, 205L, 225L, 30000L, 886L, 969L))
 
   expect_equal(cohortdataOutput,cohortdataOutput_compared)
-
 })
