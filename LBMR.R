321203ff9bd14564a3b013314efb59274059c905
Alex Chubaty <alex.chubaty@gmail.com>
2018-12-20 20:47:55 -0700

    use 'LandR' package

--- LBMR.R
+++ LBMR.R
@@ -103,7 +103,7 @@ defineModule(sim, list(
                               "and should also contain a color for 'Mixed'"),
                  sourceURL = NA),
     expectsInput("sppEquiv", "data.table",
-                 desc = "table of species equivalencies. See pemisc::sppEquivalencies_CA.", ## TODO: use LandR
+                 desc = "table of species equivalencies. See LandR::sppEquivalencies_CA.",
                  sourceURL = ""),
     expectsInput("studyArea", "SpatialPolygonsDataFrame",
                  desc = paste("multipolygon to use as the study area,",
@@ -1257,7 +1257,7 @@ CohortAgeReclassification <- function(sim) {
   }
 
   if (!suppliedElsewhere("sppEquiv", sim)) {
-    data("sppEquivalencies_CA", package = "pemisc", envir = environment()) ## TODO: sue LandR
+    data("sppEquivalencies_CA", package = "LandR", envir = environment())
     sim$sppEquiv <- as.data.table(sppEquivalencies_CA)
 
     ## By default, Abies_las is renamed to Abies_sp
@@ -1266,8 +1266,8 @@ CohortAgeReclassification <- function(sim) {
     ## add default colors for species used in model
     if (!is.null(sim$sppColors))
       stop("If you provide sppColors, you MUST also provide sppEquiv")
-    sim$sppColors <- pemisc::sppColors(sim$sppEquiv, P(sim)$sppEquivCol, ## TODO: use LandR
-                                       newVals = "Mixed", palette = "Accent")
+    sim$sppColors <- sppColors(sim$sppEquiv, P(sim)$sppEquivCol,
+                               newVals = "Mixed", palette = "Accent")
   }
 
   return(invisible(sim))
