4a6078b7c2bd5d2adfe9f3cd54f507554f6d2008
Alex Chubaty <alex.chubaty@gmail.com>
2019-05-21 10:43:40 -0700

    fix bug in ageReclassification

--- R/age-cohorts.R
+++ R/age-cohorts.R
@@ -3,20 +3,22 @@ ageReclassification <- function(cohortData, successionTimestep, stage) {
     # for spin up stage
     cohortData[age == successionTimestep + 1, age := successionTimestep]
   } else {
+    successionTimestepPlusOne <- asInteger(successionTimestep + 1)
+
     # non- spinup stage
-    targetData <- cohortData[age <= (successionTimestep + 1), ]
+    targetData <- cohortData[age <= successionTimestepPlusOne, ]
 
     # Squash multiple cohorts that regenerated within the successionTimestep
     #   into a single cohort
     targetData <- targetData[, .(ecoregionGroup = unique(ecoregionGroup),
-                                 age = asInteger(successionTimestep + 1),
+                                 age = successionTimestepPlusOne,
                                  B = sum(B, na.rm = TRUE),
                                  mortality = sum(mortality, na.rm = TRUE),
                                  aNPPAct = sum(aNPPAct, na.rm = TRUE)),
                              by = .(pixelGroup, speciesCode)]
     targetData <- targetData[, .(pixelGroup, ecoregionGroup, speciesCode, age,
                                  B, mortality, aNPPAct)]
-    cohortData <- cohortData[age >= successionTimestep + 2]
+    cohortData <- cohortData[age > successionTimestepPlusOne]
     cohortData <- rbindlist(list(cohortData, targetData))
   }
   if (isTRUE(getOption("LandR.assertions"))) {
