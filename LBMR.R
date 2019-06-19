3456b9ada1b5fb0fd023ade3f346e0d82247c519
CeresBarros <ceresvbarros@hotmail.com>
2019-06-18 17:32:41 -0700

    Revert "use activePixelIndex for plot, instead of RTMReporting"
    This reverts commit 75777a9f7db4957d0bc80ebee18e1b2e1e94abf8.

--- LBMR.R
+++ LBMR.R
@@ -1455,8 +1455,8 @@ plotSummaryBySpecies <- function(sim) {
       Plot(plot2, title = paste0("Total biomass by species\n",
                                  "across pixels"), new = TRUE)
     }
-
-    maxNpixels <- length(sim$activePixelIndex)
+    ## TODO: make sure only "active" pixels are selected
+    maxNpixels <- sum(!is.na(sim$rasterToMatchReporting[]))
     cols3 <- sim$summaryBySpecies1$cols
     names(cols3) <- sim$summaryBySpecies1$leadingType
 
