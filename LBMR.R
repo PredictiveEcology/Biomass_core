2b890d05d815a81108103f2ce9c83f0046767613
CeresBarros <ceresvbarros@hotmail.com>
2019-08-23 14:49:43 -0700

    save rasters in figures/ folder

--- LBMR.R
+++ LBMR.R
@@ -1647,16 +1647,20 @@ Save <- compiler::cmpfun(function(sim) {
   raster::projection(sim$mortalityMap) <- raster::projection(sim$ecoregionMap)
   raster::projection(sim$reproductionMap) <- raster::projection(sim$ecoregionMap)
   writeRaster(sim$simulatedBiomassMap,
-              file.path(outputPath(sim), paste("simulatedBiomassMap_Year", round(time(sim)), ".tif", sep = "")),
+              file.path(outputPath(sim), "figures",
+                        paste0("simulatedBiomassMap_Year", round(time(sim)), ".tif")),
               datatype = 'INT4S', overwrite = TRUE)
   writeRaster(sim$ANPPMap,
-              file.path(outputPath(sim), paste("ANPP_Year", round(time(sim)), ".tif", sep = "")),
+              file.path(outputPath(sim), "figures",
+                        paste0("ANPP_Year", round(time(sim)), ".tif")),
               datatype = 'INT4S', overwrite = TRUE)
   writeRaster(sim$mortalityMap,
-              file.path(outputPath(sim), paste("mortalityMap_Year", round(time(sim)), ".tif", sep = "")),
+              file.path(outputPath(sim), "figures",
+                        paste0("mortalityMap_Year", round(time(sim)), ".tif")),
               datatype = 'INT4S', overwrite = TRUE)
   writeRaster(sim$reproductionMap,
-              file.path(outputPath(sim), paste("reproductionMap_Year", round(time(sim)), ".tif", sep = "")),
+              file.path(outputPath(sim), "figures",
+                        paste0("reproductionMap_Year", round(time(sim)), ".tif")),
               datatype = 'INT4S', overwrite = TRUE)
   return(invisible(sim))
 })
