aa7e3ca29a26e7da0c48e06dd89a9775b07e3574
Eliot McIntire <eliotmci@mail.ubc.ca>
2020-10-04 00:38:13 -0700

    minor

--- tests/testthat/test-wardDispersalFunction.R
+++ tests/testthat/test-wardDispersalFunction.R
@@ -16,9 +16,9 @@ test_that("test Ward dispersal seeding algorithm", {
   reducedPixelGroupMap <- raster(xmn = 50, xmx = 50 + 99*25,
                                  ymn = 50, ymx = 50 + 99*25,
                                  res = c(100, 100), val = 2)
-  # reducedPixelGroupMap <- raster(xmn = 50, xmx = 50 + 99*300,
-  #                                ymn = 50, ymx = 50 + 99*300,
-  #                                res = c(100, 100), val = 2)
+  reducedPixelGroupMap <- raster(xmn = 50, xmx = 50 + 99*300,
+                                 ymn = 50, ymx = 50 + 99*300,
+                                 res = c(100, 100), val = 2)
   cc <- expand.grid(data.frame(a = seq(5, 99, by = 9), b = seq(5, 99, by = 9)))
   pixelindex <- (cc$a-1)*99+cc$b #121
   reducedPixelGroupMap[pixelindex] <- 1
