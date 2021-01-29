6eac33be9b3e7a23241910ecb7c82f390d695c27
Ian Eddy <ianmseddy@gmail.com>
2021-01-29 14:12:02 -0800

    reverse logic in growthAndMortality

--- Biomass_core.R
+++ Biomass_core.R
@@ -1064,7 +1064,7 @@ MortalityAndGrowth <- compiler::cmpfun(function(sim) {
     set(subCohortData, NULL, "aNPPAct", pmax(1, subCohortData$aNPPAct - subCohortData$mAge))
 
     ## generate climate-sensitivity predictions - this will no longer run if LandR pkg is the driver
-    if (P(sim)$growthAndMortalityDrivers == "LandR.CS") {
+    if (!P(sim)$growthAndMortalityDrivers == "LandR") {
       #necessary due to column joining
       if (!is.null(subCohortData$growthPred)) {
         set(subCohortData, NULL, c('growthPred', 'mortPred'), NULL)
@@ -1094,7 +1094,7 @@ MortalityAndGrowth <- compiler::cmpfun(function(sim) {
     set(subCohortData, NULL, "mortality", subCohortData$mBio + subCohortData$mAge)
 
     ## this line will return mortality unchanged unless LandR_BiomassGMCS is also run
-    if (P(sim)$growthAndMortalityDrivers == "LandR.CS") {
+    if (!P(sim)$growthAndMortalityDrivers == "LandR") {
 
       subCohortData[, mortality := pmax(0, asInteger(mortality * mortPred/100))]
       subCohortData[, mortality := pmin(mortality, B + aNPPAct)] #this prevents negative biomass, but allows B = 0 for 1 year
