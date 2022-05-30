e89c407ed4fb9711b92e10879557c90ae42c4ece
CeresBarros <cbarros@mail.ubc.ca>
2022-05-30 09:37:59 -0700

    bugfix in module metadata

--- Biomass_core.R
+++ Biomass_core.R
@@ -10,7 +10,7 @@ defineModule(sim, list(
     person("Ceres", "Barros", email = "ceres.barros@ubc.ca", role = "aut"),
     person(c("Alex", "M."), "Chubaty", email = "achubaty@for-cast.ca", role = "aut"),
     person("Ian", "Eddy", email = "ian.eddy@nrcan-rncan.gc.ca", role = c("ctb")),
-    person("Jean", "Marchal", email = "jean.d.marchal@gmail.com", role = "ctb"),
+    person("Jean", "Marchal", email = "jean.d.marchal@gmail.com", role = "ctb")
   ),
   childModules = character(0),
   version = list(Biomass_core = numeric_version("1.3.9")),
