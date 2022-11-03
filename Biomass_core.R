0d05fa84ad95ce3f2515284d275fabbb15b7be9e
CeresBarros <cbarros@mail.ubc.ca>
2022-11-03 14:37:56 -0700

    metadata: pkg bumps and re-organisation

--- Biomass_core.R
+++ Biomass_core.R
@@ -19,16 +19,17 @@ defineModule(sim, list(
   timeunit = "year",
   citation = list("citation.bib"),
   documentation = list("README.txt", "Biomass_core.Rmd"),
-  reqdPkgs = list("assertthat", "compiler", "crayon", "data.table", "dplyr", "fpCompare", "ggplot2", "grid",
-                  # "curl", "httr", ## called directly by this module, but pulled in by LandR (Sep 6th 2022).
-                  ## Excluded because loading is not necessary (just installation)
+  reqdPkgs = list("assertthat", "compiler", "crayon", "data.table",
+                  "dplyr", "fpCompare", "ggplot2", "grid",
                   "parallel", "purrr", "quickPlot", "raster", "Rcpp",
                   "R.utils", "scales", "sp", "tidyr",
-                  "PredictiveEcology/LandR@development (>= 1.1.0.9004)",
+                  # "curl", "httr", ## called directly by this module, but pulled in by LandR (Sep 6th 2022).
+                  ## Excluded because loading is not necessary (just installation)
+                  "PredictiveEcology/LandR@development (>= 1.1.0.9006)",
                   "PredictiveEcology/pemisc@development",
                   "PredictiveEcology/reproducible@development",
-                  "PredictiveEcology/SpaDES.core@development (>= 1.0.8.9000)",
-                  "PredictiveEcology/SpaDES.tools@development",
+                  "PredictiveEcology/SpaDES.core@development (>= 1.0.8.9001)",
+                  "PredictiveEcology/SpaDES.tools@development (>= 1.0.0.9001)",
                   "ianmseddy/LandR.CS@master (>= 0.0.2.0002)"),
   parameters = rbind(
     defineParameter("calcSummaryBGM", "character", "end", NA, NA,
