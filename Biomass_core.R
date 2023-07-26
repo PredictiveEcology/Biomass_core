439c42a6e4aa681878aed2d2f7d47370c62177bf
CeresBarros <cbarros@mail.ubc.ca>
2023-07-26 14:22:16 -0700

    spTransform -> projectInputs

--- Biomass_core.R
+++ Biomass_core.R
@@ -2084,7 +2084,7 @@ CohortAgeReclassification <- function(sim) {
   if (!.compareCRS(sim$studyArea, sim$rasterToMatch)) {
     warning(paste0("studyArea and rasterToMatch projections differ.\n",
                    "studyArea will be projected to match rasterToMatch"))
-    sim$studyArea <- spTransform(sim$studyArea, crs(sim$rasterToMatch))
+    sim$studyArea <- projectInputs(sim$studyArea, crs(sim$rasterToMatch))
     sim$studyArea <- fixErrors(sim$studyArea)
   }
 
